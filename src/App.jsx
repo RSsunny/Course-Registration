f6e4abd558541ff8547f5878c13b9848a5099def
RSsunny <sunnysharif154@gmail.com>
2023-09-16 13:40:36 +0600

    credit houe and remining credit show list cart. when credit limite up to 20 then show toast alert . Toast instal from react toast and setup main.jsx component.

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,54 @@ import './App.css'
 import Carts from './components/Carts/Carts'
 import Header from './components/Header/Header'
 import List from './components/List/List'
+import { toast } from 'react-toastify';
 
 
 function App() {
  const [list,setList]=useState([])
+ const [total,setTotal]=useState(0)
+ const [credit,setCredit]=useState(0)
+ const [creditRemining,setCreditRemining]=useState(20)
  console.log(list);
+ console.log(total);
+ console.log(credit);
  const handleAdd=cart=>{
-  const newList=[...list,cart]
-  setList(newList)
+  const findlist=list.find(data=>data.title_name==cart.title_name)
+  if(findlist){
+    return   toast.error('cart alrady Exist', {
+      position: toast.POSITION.TOP_CENTER, 
+      
+      autoClose: 3000
+    });
+  
+  }
+  else{
+    const newCredit=credit+cart.credits
+    if(newCredit>20){
+      return  toast.error('credit over', {
+        position: toast.POSITION.TOP_CENTER, 
+        autoClose: 3000
+      });
+    }
+    const newTotal=total+ cart.price
+    const newList=[...list,cart]
+    const totalRemaining=creditRemining-cart.credits
+    setCredit(newCredit)
+    setCreditRemining(totalRemaining)
+    setTotal((newTotal))
+    setList(newList)
+  }
+  
   
  }
-
+ 
   return (
     <>
     <div className='max-w-7xl mx-auto px-5 '>
         <Header></Header>
         <div className='flex flex-col-reverse md:flex-row gap-5'>
             <Carts handleAdd={handleAdd}></Carts>
-            <List list={list}></List>
+            <List list={list} total={total} creditRemining={creditRemining} credit={credit} ></List>
         </div>
     </div>
     </>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,21 +1,25 @@
 import PropTypes from 'prop-types';
 
-const List = ({list}) => {
+const List = ({list,total,credit,creditRemining}) => {
+    
     return (
-        <div className="md:w-1/4">
-            <h1>Credit Hour Remaining 7 hr</h1>
-            <h1>Course Name</h1>
-            <ul>
+        <div className="md:w-1/4 bg-white text-black p-4">
+            <h1 className='text-lg font-bold text-[#2F80ED] border-b border-black py-3'>Credit Hour Remaining {creditRemining}hr</h1>
+            <h1 className='text-xl font-bold my-5'>Course Name</h1>
+            <ul className='border-b border-black pb-5'>
                 {
-                    list.map((data,i)=><h1 key={i}>{i+1} {data.title_name}</h1>)
+                    list.map((data,i)=><h1 key={i} className='text-base font-normal border p-2 my-2 rounded-md'>{i+1}. {data.title_name}</h1>)
                 }
             </ul>
-            <h1>Total Credit Hour : 13</h1>
-            <h1>Total Price : 48000 USD</h1>
+            <h1 className='text-base font-medium my-3 border-b border-black pb-3'>Total Credit Hour : {credit}</h1>
+            <h1 className='text-base font-medium my-3'>Total Price : {total} USD</h1>
         </div>
     );
 };
 List.propTypes={
-    list:PropTypes.array.isRequired
+    list:PropTypes.array.isRequired,
+    total:PropTypes.number.isRequired,
+    credit:PropTypes.number.isRequired,
+    creditRemining:PropTypes.number.isRequired,
 }
 export default List;
